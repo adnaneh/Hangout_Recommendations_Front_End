b52d8b1618a7b631806076fee44f0ac72d413b32
Zhuofan YU <zhuofan.yu@student-cs.fr>
2020-01-16 22:27:10 +0100

    resize the event card

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MessageService } from '../communicator/message.service';
-import { Subscription } from 'rxjs';
+import { CommunicatorService } from '../communicator/communicator.service';
 import { ActivatedRoute } from '@angular/router';
 import data from '../assets/99746';
 import { Event } from '../format';
@@ -17,13 +16,16 @@ export class EventDetailComponent implements OnInit {
   event_id: number;
   event: Event;
 
-  constructor(private _activatedRoute: ActivatedRoute) {
-    this.event = data;
+  constructor(private _activatedRoute: ActivatedRoute, private communicatorService: CommunicatorService) {
+    this.event = data;  // uncomment for offline test
   }
 
   ngOnInit() {
     this.event_id = this._activatedRoute.snapshot.params['id'];
-  }
+    /*this.communicatorService.getEvent(this.event_id.toString())
+      .subscribe(() => this.event);*/
+  }   // uncomment for online situation
+
 
   showEventId() {
     console.log(this.event_id);
