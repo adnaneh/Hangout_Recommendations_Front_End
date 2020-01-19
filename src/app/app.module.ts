afd76c42780a5e0eca7f9dad2abbd98bfd4cd31b
Zhuofan YU <zhuofan.yu@student-cs.fr>
2020-01-20 00:25:06 +0100

    add router in welcome page to the signup page, update the sign in page; add globalInfo service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     HttpClientModule,
     MatExpansionModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: '', component: EventComponent },
       { path: 'login', component: LoginComponent },
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommunicatorService } from '../communicator/communicator.service'
-import { LoginInfo } from '../format'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginInfo, LoginResp } from '../format'
+import { NgForm, FormGroup } from '@angular/forms';
+import { GlobalInfoService } from '../communicator/global-info.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -9,45 +12,45 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
   loginInfo: LoginInfo;
   loginForm: FormGroup;
   loading = false;
   submitted = false;
   //returnUrl
   response: string = '';
+  loginResp: any;
 
-  constructor(private communicatorService: CommunicatorService) {
-
-  }
-
-
-
-
-
+  constructor(private communicatorService: CommunicatorService, private globalInfo: GlobalInfoService, private router: Router) { }
 
 
 
   ngOnInit() {
-    this.loginInfo = {
+    /*this.loginInfo = {
       'unique_key': '1',
       'pword': 'hello password'
+    }*/
+  }
+
+
+
+  onSubmit(f: LoginInfo) {
+    this.login(f);
+    console.log(f);  // { first: '', last: '' }
+  }
+
+  login(f: LoginInfo) {
+    this.communicatorService.loginUser(f)
+      .subscribe(resp => {
+        this.loginResp = resp;
+        console.log(this.loginResp)
+      });
+
+    if (this.loginResp['state'] == true) {
+      this.globalInfo.login(this.loginResp['user_online']);
+      this.router.navigate(['']);
+    } else {
+      console.log(this.loginResp['description']);
     }
-  }
-
-  login() {
-    this.communicatorService
-      .loginUser(this.loginInfo).subscribe();
-  }
-
-  onSubmit() {
-    this.communicatorService
-      .loginUser(this.loginInfo).subscribe(resp => console.log(resp));
-  }
-
-  getResp() {
-    this.communicatorService
-      .getLoginUser().subscribe();
   }
 
 }
