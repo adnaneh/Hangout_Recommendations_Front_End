dfe021e276e235a65642dd378a02a678c5ede335
Adnane Hamid <adnane.hamid@student.ecp.fr>
2020-02-03 19:58:39 +0100

    test

--- src/app/communicator/communicator.service.ts
+++ src/app/communicator/communicator.service.ts
@@ -141,6 +141,7 @@ export class CommunicatorService {
     }
     // return an observable with a user-facing error message
     return throwError(
+      
       'Something bad happened; please try again later.');
   };
 
