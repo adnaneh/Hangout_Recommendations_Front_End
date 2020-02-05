741913cd061c527d613c2b0fc0380bd40a980f78
Zhuofan YU <zhuofan.yu@student-cs.fr>
2020-02-05 20:41:49 +0100

    delete useless buttons

--- src/app/communicator/communicator.service.ts
+++ src/app/communicator/communicator.service.ts
@@ -31,7 +31,7 @@ export class CommunicatorService {
     private AES: AESService
   ) { }
 
-  readonly url_root = 'http://10.55.154.23:8080/';
+  readonly url_root = 'http://192.168.56.1:8080/';
   readonly url_api = 'api/v1.0';
   readonly serverUrl = this.url_root + this.url_api;
   readonly eventsUrl = '/Events';
